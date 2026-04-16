0b3efdc506a421bbe8153ddd16d95364198f7298
givey420 <afk.givey@protonmail.com>
2026-04-16 12:25:24 +0300

    projects: add givey.zip itself as a project tile (links to github source)

--- js/projects.js
+++ js/projects.js
@@ -6,4 +6,11 @@ export const projects = [
     url: 'https://donuttrade.com',
     thumbnail: null,
   },
+  {
+    id: 'givey-zip',
+    title: 'givey.zip',
+    description: 'This site. Static HTML/CSS/ESM JavaScript, no framework, card-flip projects view, live Discord presence, random theme pairs per load.',
+    url: 'https://github.com/givey999/givey.zip',
+    thumbnail: null,
+  },
 ]
