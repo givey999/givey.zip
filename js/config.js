af6f1d53214ae5c6cc4bf47ea050fd3dbe8d1503
givey420 <afk.givey@protonmail.com>
2026-04-23 17:23:15 +0300

    about: rewrite to be warmer, less geeky, add cats

--- js/config.js
+++ js/config.js
@@ -13,7 +13,9 @@ export const BIO_TEXT = 'wanna be web developer'
 
 // Edit these paragraphs to change the "about" panel on the card back.
 export const ABOUT = [
-  "hey — i'm givey.",
-  "i build things on the web, mostly in typescript + node. currently obsessed with dst; previously shipped donuttrade. this site is my excuse to practice css animations.",
-  "based in romania. find me on the socials on the front.",
+  "hey, i'm givey. from romania.",
+  "i make things online — apps, websites, small tools for problems i happen to have. usually i'm the first user.",
+  "right now i'm building dst, a little app that lets you keep your files private, encrypted, and out of the way. before that was donuttrade, a safe way for people on a minecraft server to trade without getting scammed. and before that, this site — part homepage, part me messing around with animations.",
+  "when i'm not making something i'm either in minecraft or defending my keyboard from a cat.",
+  "socials on the front. i answer faster on discord.",
 ]
